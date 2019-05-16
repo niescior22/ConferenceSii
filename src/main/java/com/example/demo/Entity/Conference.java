bfef0eba42ffe9bc474ce44140c717c69514a7de
jakub <jakubniescior.gdynia@gmail.com>
2019-05-16 12:07:42 +0200

    Adding Entity Classes

---
+++ src/main/java/com/example/demo/Entity/Conference.java
@@ -0,0 +1,112 @@
+package com.example.demo.Entity;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Conference {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private  Long id;
+
+
+    private String name;
+
+    private LocalDate date;
+
+    private String startTime;
+
+    private String endTime;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "conferences_user",joinColumns = @JoinColumn(name = "user_id"),inverseJoinColumns = @JoinColumn(name ="conference_id"))
+    private Set<User> users= new LinkedHashSet<>();
+
+
+    public Conference() {
+    }
+
+    public Conference(String name, LocalDate date, String startTime, String endTime, Set<com.example.demo.Entity.User> users) {
+        this.name = name;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.users = users;
+    }
+
+    public Conference(String name, LocalDate date, String startTime, String endTime) {
+        this.name = name;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public Set<com.example.demo.Entity.User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<com.example.demo.Entity.User> users) {
+        this.users = users;
+    }
+    public int Setsize(Set<User> users){
+        int size = users.size();
+        return size;
+
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conference that = (Conference) o;
+        return Objects.equals(id, that.id);
+    }
+}
---
+++ src/main/java/com/example/demo/Entity/User.java
@@ -0,0 +1,91 @@
+package com.example.demo.Entity;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String login;
+    @Email
+    private String email;
+
+    @ManyToMany(mappedBy = "users",fetch = FetchType.EAGER )
+    private Set<Conference> conferences = new HashSet<>();
+
+
+
+    public User(String login, String email) {
+        this.login = login;
+        this.email = email;
+    }
+
+    public User(String login, String email, Set<Conference> conferences) {
+        this.login = login;
+        this.email = email;
+        this.conferences = conferences;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Conference> getConferences() {
+        return conferences;
+    }
+
+    public void setConferences(Set<Conference> conferences) {
+        this.conferences = conferences;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return login;
+    }
+}
+
